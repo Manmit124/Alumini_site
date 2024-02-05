7da27b3c0819c88e09ef699ee1888e296f3b5443
Manmit Tiwade <112580293+Manmit124@users.noreply.github.com>
2024-02-05 21:46:11 +0530

    changing in alumini card

--- app/(section)/Alumini/page.jsx
+++ app/(section)/Alumini/page.jsx
@@ -1,40 +1,57 @@
-"use client"
-import Userprofile from '@/hooks/userProfile'
-import { Link } from 'lucide-react'
-import Image from 'next/image'
-import React from 'react'
+"use client";
+import Userprofile from "@/hooks/userProfile";
+import { Link } from "lucide-react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
 const Page = () => {
-  const {loading, data}=Userprofile();
+  const [Aluminis, setAluminis] = useState([]);
+  const { loading, data } = Userprofile();
+  useEffect(() => {
+    fetchAlumini();
+  }, []);
+  const fetchAlumini = async () => {
+    const response = await fetch("/api/users");
+    const FetchAlumini = await response.json();
+    console.log(FetchAlumini);
+    setAluminis(FetchAlumini);
+  };
   return (
-    <section className="mt-8 max-w-6xl mx-auto">
+    <div className="mt-8   justify-center">
       <div className="lg:py-5 mx-auto text-center flex flex-col items-center">
-  
-
-      <h1 className="text-3xl font-bold text-white sm:text-6xl lg:my-3">Alumini&apos s</h1>
-
+        <h1 className="text-3xl font-bold text-white sm:text-6xl lg:my-3">
+          Alumini&apos s
+        </h1>
       </div>
+<div className="   flex-row  justify-center gap-2 grid grid-rows-1 grid-cols-4 ml-6">
 
+      {Aluminis &&
+        Aluminis?.map((Alumini,index) => (
+          <>
+            <div key={index} className=" Alumini-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9] gap-x-2  flex flex-row w-[20rem]  h-[8rem] mt-2 ">
+              <div className="  rounded-full overflow-hidden">
+                <Image
+                  width={100}
+                  height={120}
+                  alt="Users image"
+                  src={Alumini?.image}
+                  className="   w-22 h-22 "
+                />
+              </div>
+              <div className=" flex flex-col ">
+                <h1 className=" text-2xl text-white">{Alumini.FullName}</h1>
+                <p className=" text-white   text-sm t">
+                  {" "}
+                  Passionate Fullstack web developer is with great vision to
+                  change my life and world also
+                </p>
+              </div>
+            </div>
+          </>
+        ))}
+</div>
+    </div>
+  );
+};
 
-      <div className=' py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9] gap-x-2  flex flex-row w-[20rem]'>
-      <div>
-
-    <Image
-      width={120}
-      height={120}
-      alt='Users image'
-      src={data.image}
-      className=' rounded-xl  '
-    />
-      </div>
-      <div className=' flex flex-col '>
-      <h1 className=' text-white  text-xl font-bold '>Manmit Tiwade</h1>
-      <p className=' text-white  text-sm'> Passionate Fullstack web developer is with great vision to change my life and world also</p>
-      </div>
-   
-      </div>
-      </section>
-  )
-}
-
-export default Page
+export default Page;
---
+++ app/api/users/route.js
@@ -0,0 +1,13 @@
+import { User } from "@/models/User";
+import { ConnectDB } from "@/utils/ConnectDB";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    ConnectDB();
+    const users = await User.find();
+    return NextResponse.json(users);
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to get users" });
+  }
+}
